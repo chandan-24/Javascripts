c1561f006e47c567d0e46276fdce1f7515748fac
Chandan Kumar <chandan.kumar9503@gmail.com>
2019-06-12 22:53:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+/*************************** 
+*    Variables and Data types 
+*/
+
+// variables must start with alphabets or with '$' or '_'
+// variables does not contain special charcters other than '$' or '_'
+/*
+console.log("hello js!!!");
+
+var firstName = "chandan";
+console.log(firstName);
+
+var lastName = "Kumar";
+console.log(lastName);
+
+var age = 23;
+console.log(age);
+
+var job
+console.log(job)
+
+job = 'teacher'
+console.log(job)
+*/
+
+/*************************** 
+*    Variable mutation and type coercion
+*/
+/*
+var name = 'john';
+var age = 28;
+
+// Type coercion
+console.log(name + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' ' + ' is a ' + age + ' yrar old ' + job + '. Is he married? '+ isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(name + ' ' + ' is a ' + age + ' yrar old ' + job + '. Is he married? '+ isMarried);
+
+var lName = prompt("Whats his last name?");
+console.log(name + ' ' + lName);
+*/
+
+
+
+/*************************** 
+*    Js operators
+*/
+/*
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof "hey! js");
+var x;
+console.log(typeof x);
+*/
+
+/*************************** 
+*    Js operators
+*/
